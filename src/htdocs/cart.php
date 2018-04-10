f65baa1ca90f060df4d42699db3822eaf91d8dda
t_nakajima <t_nakajima@bbmedia.co.jp>
2018-04-11 01:43:18 +0900

    mvcを意識してコードを修正

--- src/htdocs/cart.php
+++ src/htdocs/cart.php
@@ -11,7 +11,18 @@ $data = array();
 $user_name = isset($_SESSION['login_name']) ? $_SESSION['login_name']: NULL;
 
 
-$data['user_name'] = $user_name;
+//カートの商品一覧の情報諸々を頑張って取得する。もっとスマートにできる？
+$item_id = array();
+$item_id = get_itemId_from_cart($pdo,$user_name);
+$product_id = get_productId_from_stock($pdo,$item_id);
+$cart_list_info = array();
+foreach ($product_id as $num){
+    foreach ($num as $id){
+        array_push($cart_list_info ,get_cart_item_info($pdo,$id));
+    }
+}
+
+$data['cart_list_info'] = $cart_list_info;
 
 $amount_change = isset($_POST['amount_change']) ? $_POST['amount_change'] : NULL;
 
--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -7,19 +7,19 @@ if($login_flag === false){
     header('location:' . LOGIN_PAGE);
 }
 
+$products_info = get_product_info($pdo);
+$data['products_info'] = $products_info;
+
 $_POST = escape($_POST);
 $purchase_btn = isset($_POST['purchase_btn']) ? $_POST['purchase_btn'] : NULL;
 $login_name = isset($_SESSION['login_name']) ? $_SESSION['login_name'] : NULL;
 
 if($purchase_btn){
-
     $post_data=array(
         'login_name'=>$login_name,
         'product_id'=>$purchase_btn
     );
-
     insert_or_update_cart($pdo,$post_data);
-
 }
 
 $view = view('index.php',$data);
