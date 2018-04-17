89d562ce5da15a6ad26ae1127d0c51158307f30d
t_nakajima <t_nakajima@bbmedia.co.jp>
2018-04-17 15:16:36 +0900

    commit for imac

--- src/htdocs/system/paypalConfig.php
+++ src/htdocs/system/paypalConfig.php
@@ -1,6 +1,6 @@
 <?php
 //Client Id of REST app
-define("CLIENT_ID", "");
+define("CLIENT_ID", "AV7fxXw9dBBYXPyuAXj_7LDO2sTUyStav-T_xkolE-H-ieP7deV203IgYOvPviVdXCjYTeAsuoUMCyvu");
 
 //ButtonSource Tracker Code
 define("SBN_CODE","PP-DemoPortal-EC-JSV4-php-REST");
